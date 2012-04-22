d22b7d077472974f3c81cfe677c3bede5da5b016
Eric Gach <eric@php-oop.net>
2012-04-22 02:01:13 -0500

    Making working about section.

--- src/desk-changer/desk-changer.c
+++ src/desk-changer/desk-changer.c
@@ -5,14 +5,37 @@
 #include "desk-changer.h"
 
 // Function defs
+void desk_changer_about();
 void desk_changer_init_main_window();
 void desk_changer_init_tray_icon();
-void desk_changer_show_tray_menu();
+void desk_changer_show_tray_menu(GtkStatusIcon *, guint, guint, gpointer);
 void desk_changer_wallpaper_next();
 void desk_changer_wallpaper_previous();
 
 // Variable defs
 GtkWidget *desk_changer_window = NULL, *desk_changer_tray_menu = NULL;
+
+void desk_changer_about()
+{
+	GtkWidget *about;
+	GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file("/home/eric/Projects/desk-changer/resources/desk-changer.png", NULL);
+	const gchar *authors[] = {
+		"Eric Gach &lt;eric@php-oop.net&gt;",
+		NULL
+	};
+
+	about = gtk_about_dialog_new();
+	gtk_about_dialog_set_program_name(GTK_ABOUT_DIALOG(about), "DeskChanger");
+	gtk_about_dialog_set_version(GTK_ABOUT_DIALOG(about), DESK_CHANGER_VERSION);
+	gtk_about_dialog_set_authors(GTK_ABOUT_DIALOG(about), authors);
+	gtk_about_dialog_set_license_type(GTK_ABOUT_DIALOG(about), GTK_LICENSE_GPL_2_0);
+	gtk_about_dialog_set_logo(GTK_ABOUT_DIALOG(about), pixbuf);
+	gtk_about_dialog_set_comments(GTK_ABOUT_DIALOG(about), "DeskChanger is a simple application that allows you to configure images to be rotated on your desktop background at the interval specified by you.");
+	g_object_unref(G_OBJECT(pixbuf));
+	pixbuf = NULL;
+	gtk_dialog_run(GTK_DIALOG(about));
+	gtk_widget_destroy(GTK_WIDGET(about));
+}
 
 void desk_changer_init_main_window()
 {
@@ -50,6 +73,10 @@ void desk_changer_show_tray_menu(GtkStatusIcon *status_icon, guint button, guint
 		g_signal_connect(G_OBJECT(item), "activate", G_CALLBACK(desk_changer_wallpaper_next), NULL);
 
 		gtk_menu_shell_append(GTK_MENU_SHELL(desk_changer_tray_menu), gtk_separator_menu_item_new());
+
+		item = gtk_menu_item_new_with_label("About");
+		gtk_menu_shell_append(GTK_MENU_SHELL(desk_changer_tray_menu), item);
+		g_signal_connect(G_OBJECT(item), "activate", G_CALLBACK(desk_changer_about), NULL);
 
 		item = gtk_menu_item_new_with_label("Exit");
 		gtk_menu_shell_append(GTK_MENU_SHELL(desk_changer_tray_menu), item);
